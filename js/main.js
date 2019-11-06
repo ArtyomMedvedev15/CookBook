66f97668fbf24860ceb3422cdbdb84b0b1f7a0a9
Tim01Bro <arthaos14@gmail.com>
2019-11-07 00:34:41 +0300

    Best Recipes section

--- js/main.js
+++ js/main.js
@@ -7,4 +7,14 @@ $(function(){
         infinite: true
  
     });
-});
+    for(var i = 1;i<=6;i++){
+    $("#rateYo"+i).rateYo({
+        normalFill: "#A0A0A0",
+        starWidth:"15px",
+        numStars:5,
+        rating:"80%",
+        readOnly:true
+      });
+    }
+      
+ });
